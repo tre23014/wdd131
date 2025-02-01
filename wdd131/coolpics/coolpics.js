0b30621184c4c5d7c67708bde68016cb2ba171bc
tre23014 <tre23014@byui.edu>
2025-01-31 18:06:24 -0700

    Cool pics

---
+++ wdd131/coolpics/coolpics.js
@@ -0,0 +1,31 @@
+const menuButton = document.querySelector(".menu-button");
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 10000) {
+        menu.classList.remove("hide");
+    } else {
+        menu.classList.add("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+function viewerTemplate(pic, alt) {
+    return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+
+function viewHandler(event) {
+
+}
